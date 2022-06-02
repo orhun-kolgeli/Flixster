b7a1cd8c4de6a825db4ca46441fa9fa5f1f81e90
Orhun Kolgeli <74283397+orhun-kolgeli@users.noreply.github.com>
2022-06-02 16:50:08 -0700

    Create Movie class

--- app/src/main/java/com/orhunkolgeli/flixster/models/Movie.java
+++ app/src/main/java/com/orhunkolgeli/flixster/models/Movie.java
@@ -1,4 +1,14 @@
 package com.orhunkolgeli.flixster.models;
 
+import org.json.JSONObject;
+
 public class Movie {
+    String posterPath;
+    String title;
+    String overview;
+
+    // Constructor
+    public Movie(JSONObject jsonObject) {
+        posterPath = jsonObject.getString("poster_path");
+    }
 }
