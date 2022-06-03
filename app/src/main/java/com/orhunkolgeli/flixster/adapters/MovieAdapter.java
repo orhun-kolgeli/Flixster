f0f6e15d2b032fa0b728b8bfa64dda8a758476ca
Orhun Kolgeli <74283397+orhun-kolgeli@users.noreply.github.com>
2022-06-03 15:26:40 -0700

    Implement required Flixster functionality

--- app/src/main/java/com/orhunkolgeli/flixster/adapters/MovieAdapter.java
+++ app/src/main/java/com/orhunkolgeli/flixster/adapters/MovieAdapter.java
@@ -1,6 +1,9 @@
 package com.orhunkolgeli.flixster.adapters;
 
 import android.content.Context;
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,8 +14,11 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
+import com.orhunkolgeli.flixster.MovieDetailsActivity;
 import com.orhunkolgeli.flixster.R;
 import com.orhunkolgeli.flixster.models.Movie;
+
+import org.parceler.Parcels;
 
 import java.util.List;
 
@@ -28,7 +34,8 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> 
     // Usually involves inflating a layout from XML and returning the holder
     @NonNull
     @Override
-    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) { // expensive
+        Log.d("MovieAdapter", "onCreateViewHolder");
         // Inflating is expensive
         View movieView = LayoutInflater.from(context).inflate(R.layout.item_movie, parent, false);
         return new ViewHolder(movieView);
@@ -36,7 +43,8 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> 
 
     // Involves populating data into the item through holder
     @Override
-    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) { // relatively cheap
+        Log.d("MovieAdapter", "onBindViewHolder " + position);
         // Get the movie at the given position
         Movie movie = movies.get(position);
 
@@ -54,7 +62,9 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> 
     }
 
     // Representation of a row in the app
-    public class ViewHolder extends RecyclerView.ViewHolder {
+    // class *cannot* be static
+    // implements View.OnClickListener
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
         TextView tvTitle;
         TextView tvOverview;
@@ -65,14 +75,49 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> 
             tvTitle = itemView.findViewById(R.id.tvTitle);
             tvOverview = itemView.findViewById(R.id.tvOverview);
             ivPoster = itemView.findViewById(R.id.ivPoster);
-
+            // add this as the itemView's OnClickListener
+            itemView.setOnClickListener(this);
         }
 
         public void bind(Movie movie) {
             tvTitle.setText(movie.getTitle());
             tvOverview.setText(movie.getOverview());
             // Display image with the Glide Library
-            Glide.with(context).load(movie.getPosterPath()).into(ivPoster);
+            // alternative implementation: String imageUrl;
+            if (context.getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+                //imageUrl = movie.getBackdropPath();
+                Glide.with(context)
+                        .load(movie.getBackdropPath())
+                        .placeholder(R.drawable.flicks_backdrop_placeholder)
+                        .into(ivPoster);
+            }
+            else {
+                //imageUrl = movie.getPosterPath();
+                Glide.with(context)
+                        .load(movie.getPosterPath())
+                        .placeholder(R.drawable.flicks_movie_placeholder)
+                        .into(ivPoster);
+            }
+
+
+        }
+
+        // when the user clicks on a row, show MovieDetailsActivity for the selected movie
+        @Override
+        public void onClick(View v) {
+            // gets item position
+            int position = getAdapterPosition();
+            // make sure the position is valid, i.e. actually exists in the view
+            if (position != RecyclerView.NO_POSITION) {
+                // get the movie at the position, this won't work if the class is static
+                Movie movie = movies.get(position);
+                // create intent for the new activity
+                Intent intent = new Intent(context, MovieDetailsActivity.class);
+                // serialize the movie using parceler, use its short name as a key
+                intent.putExtra(Movie.class.getSimpleName(), Parcels.wrap(movie));
+                // show the activity
+                context.startActivity(intent);
+            }
         }
     }
 }
--- app/src/main/java/com/orhunkolgeli/flixster/models/Movie.java
+++ app/src/main/java/com/orhunkolgeli/flixster/models/Movie.java
@@ -3,21 +3,30 @@ package com.orhunkolgeli.flixster.models;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.parceler.Parcel;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class Movie {
+@Parcel // annotation indicates class is Parcelable
+public class Movie { // no private qualifiers
+    String backdropPath;
     String posterPath;
     String title;
     String overview;
+    Double voteAverage;
 
-    // Constructor
+    // No-arg, empty constructor required for Parceler
+    public Movie() {}
+
+    // Actual constructor
     public Movie(JSONObject jsonObject) throws JSONException {
         // the caller is responsible for handling the exception
+        backdropPath = jsonObject.getString("backdrop_path");
         posterPath = jsonObject.getString("poster_path");
         title = jsonObject.getString("title");
         overview = jsonObject.getString("overview");
+        voteAverage = jsonObject.getDouble("vote_average");
     }
 
     public static List<Movie> fromJsonArray(JSONArray movieJsonArray) throws JSONException {
@@ -33,11 +42,20 @@ public class Movie {
         return String.format("https://image.tmdb.org/t/p/w342/%s", posterPath);
     }
 
+    public String getBackdropPath() {
+        // Hardcode the size as a shortcut (width = 342)
+        return String.format("https://image.tmdb.org/t/p/w342/%s", backdropPath);
+    }
+
     public String getTitle() {
         return title;
     }
 
     public String getOverview() {
         return overview;
+    }
+
+    public Double getVoteAverage() {
+        return voteAverage;
     }
 }
